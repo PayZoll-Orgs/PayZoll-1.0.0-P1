cf25c35517c95363fb397c06515666963655a214
sahil58555 <sahil.saxena.58555@gmail.com>
2025-03-20 21:04:43 +0530

    backend-url update

--- Frontend/src/constant/domain.js
+++ Frontend/src/constant/domain.js
@@ -1,2 +1,2 @@
-export const backendDomain = "https://payzoll-hackathon-build.onrender.com";
+export const backendDomain = "https://payzoll-core.onrender.com";
 // export const backendDomain = "http://localhost:5001";
